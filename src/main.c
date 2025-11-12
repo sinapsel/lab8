eab58cbde92ab3e15adea3e25ddfdef8368c7961
Mark <masimanovskiy@mephi.ru>
2025-11-12 22:13:09 +0300

    main (demo) refactoring

--- src/main.c
+++ src/main.c
@@ -5,23 +5,42 @@
 #include "circle.h"
 #include "rect.h"
 
-int main()
-{
-
-    // инкапсуляция
+/**
+ * @brief Пример инкапсуляции
+ * 
+ */
+void incapsulation_example() {
     point2_t *p = pnew();
     psetx(p, 2);
-    // p->
+    // p->x = 2 // запрещено!
+    printf("P(%d, %d)\n", pgetx(p), pgety(p));
+}
 
-    printf("%g", pgetx(p));
-
-    /// наследование
+/**
+ * @brief Пример наследования
+ * 
+ */
+void inheritance_example() {
     circle_t *c = circle_create(1, 1, 2);
-    rect_t *r = rect_create(1, 1, 2, 3);
+    rect_t *r   = rect_create(1, 1, 2, 3);
 
     move((shape_t *)c, 2, 4);
     move((shape_t *)r, 3, -1);
+}
+
+void dyn_polymorphism_example() {
+    circle_t *c = circle_create(1, 1, 2);
+    rect_t *r   = rect_create(1, 1, 2, 3);
 
     draw((const shape_t *)c);
     draw((const shape_t *)r);
+}
+
+int main()
+{
+
+    incapsulation_example();
+    inheritance_example();
+    dyn_polymorphism_example();
+    
 }
